e410f1bc7f3d0463d724844ff036ae1e31699aa5
jusufjulma <35254483+jusufjulma@users.noreply.github.com>
2018-08-13 02:42:04 +0300

    Create index.js

---
+++ osa1/1.6/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx'
+import App2 from './App2.jsx'
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App2 />, document.getElementById('root2'));
