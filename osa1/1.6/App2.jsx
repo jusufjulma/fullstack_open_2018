1597266b2eeeca38e11b1857facadf98612ae528
jusufjulma <35254483+jusufjulma@users.noreply.github.com>
2018-08-29 02:15:02 +0300

    Final fixes
    Added comments and removed some clutter

--- osa1/1.6/App2.jsx
+++ osa1/1.6/App2.jsx
@@ -1,48 +1,46 @@
 import React from 'react'
 
-class App2 extends React.Component {
+class App2 extends React.Component {            // declaring as class
   constructor(props) {
     super(props)
     this.state = {
-      selected: Math.round(Math.random()*5),
-      votes: [0, 0, 0, 0, 0, 0],
-      mostVoted: null
-    }
-  }
+      selected: Math.round(Math.random()*5),    // boring to see same anecdote
+      votes: [0, 0, 0, 0, 0, 0],                // array that stores votes
+      mostVoted: null                           // explanatory name. Start value
+    }                                           // for styling purposes; can't
+  }                                             // be zero due to array indexes
 
-  nextAnecdote = () => {
-    let random = Math.round(Math.random()*5);
+  nextAnecdote = () => {                        // switches between anecdotes
+    let random = Math.round(Math.random()*5);   // generates new value for select
     while (random === this.state.selected){     // it's pseudorandom anyways
       random = Math.round(Math.random()*5);
     }
-    this.setState({
+    this.setState({                             // applies the new value
       selected: random
     })
   }
-  voteAnecdote = (asd) => {
-
+  voteAnecdote = () => {              // adds vote to array "votes"
     let vote = [...this.state.votes]  // creates a copy of state array
     vote[this.state.selected]++       // which then gets modified
     this.setState({                   // and overwritten to state
       votes: vote                     // here
     })
 
-    this.winningAnecdote(vote)     // executes the function which
-  }                                // figures out the most voted one
-  winningAnecdote = (votearray) => {
-    let mostVotes = this.state.mostVoted
-    for (let i = 0; i < votearray.length; i++) {
+    this.winningAnecdote(vote)        // executes the function which
+  }                                   // figures out the most voted one
+  winningAnecdote = (votearray) => {  // gets the new array as an argument
+    let mostVotes = this.state.mostVoted          // checks current state
+    for (let i = 0; i < votearray.length; i++) {  //checks every anecdote
       if (votearray[i] > votearray[mostVotes] || mostVotes === null){
-        mostVotes = i
-      }
-    }
-    this.setState({
+        mostVotes = i                 // if value in array is bigger than
+      }                               // the value of current iteration value
+    }                                 // it updates the value
+    this.setState({                   // and then sets it to the correct state
       mostVoted: mostVotes
     })
-
   }
 
-  render() {
+  render() {                          // render part of app
 
     const anecdotes = [
       'If it hurts, do it more often',
@@ -52,7 +50,7 @@ class App2 extends React.Component {
       'Premature optimization is the root of all evil.',
       'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
     ]
-    const divStyle = {
+    const divStyle = {                // couple variables (constants) for styling
       position: "absolute",
       top: "0px",
       left: "300px",
@@ -80,23 +78,23 @@ class App2 extends React.Component {
       top: "320px"
     }
 
-    const button = (whichFunction, label) => {
+    const button = (whichFunction, label) => {    // function for buttons
       return <button style={buttonStyle} onClick={whichFunction}>{label}</button>
     }
-    const mostVoted = (votes, mostVoted) => {
-      if (mostVoted === null) {
+    const mostVoted = (votes, mostVoted) => {     // function for dipslaying
+      if (mostVoted === null) {                   // or not displaying
         return
       }
-      return(
+      return(                                     // the most voted anecdote
         <div>
           <h2>Anecdote with most votes:</h2>
           <p style={anecdoteStyle}>{anecdotes[mostVoted]}</p>
           <p>Has been voted {votes[mostVoted]} times</p>
         </div>
-      )
+      )                                           // and its vote count
     }
 
-    return (
+    return (                                      // visible part; creating DOM
       <div style={divStyle}>
           <div>
             <h1>Anekdootit</h1>
@@ -106,7 +104,6 @@ class App2 extends React.Component {
             <div style={buttonContainer}>
               {button(this.nextAnecdote, "Next anecdote")}
               {button(this.voteAnecdote, "Vote")}
-              {/* {button(this.winningAnecdote, "WHO WINS")} */}
             </div>
           </div>
         <div style={voteScreen}>
@@ -117,4 +114,4 @@ class App2 extends React.Component {
   }
 }
 
-export default App2;
+export default App2;                               // EOF
